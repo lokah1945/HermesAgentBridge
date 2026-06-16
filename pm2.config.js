5bc765c6ab87a00cd77172476fed930dc58ff19e
lokah1945 <lokah2150@gmail.com>
2026-06-16 18:50:48 +0700

    chore: reinforce pm2 config and configure vitest target folder

--- pm2.config.js
+++ pm2.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'hermes-server',
+      name: 'hermes-ilma',
       script: 'dist/server/index.js',
+      watch: false,
+      max_memory_restart: '200M',
       env: {
         NODE_ENV: 'production'
       }
